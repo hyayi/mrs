da9569b3085fa1b9afd7831992d65e74bd8535fe
mongshel <66589933+mongshel@users.noreply.github.com>
2023-02-06 15:01:34 +0900

    add vit paper

--- models/vit_multi.py
+++ models/vit_multi.py
@@ -49,6 +49,42 @@ class VitMultiModalProbConcat(nn.Module):
         return x
     
 
+class VitMultiModalPaper(nn.Module):
+    def __init__(self, clinical_feature_len, num_classes=2,img_size=(64,256,256), patch_size=(8,32,32) ) -> None:
+        super().__init__()
+        
+        self.backbone = ViT(in_channels=1, img_size=img_size,patch_size=patch_size, pos_embed='conv',classification=False)
+        self.fc1 = nn.Sequential(nn.Linear(768, 256), 
+                                 nn.ReLU(inplace=True),
+                                 nn.Dropout(0.2),
+                                 nn.Linear(256, 128),
+                                 nn.ReLU(inplace=True))
+        
+        self.fc2 = nn.Sequential(
+                            nn.Linear(clinical_feature_len, 11), 
+                            nn.ReLU(inplace=True),
+                            nn.Linear(11,10), 
+                            nn.ReLU(inplace=True),
+                            nn.Dropout(0.2),
+                            nn.Linear(10, 10),
+                            nn.ReLU(inplace=True))
+
+
+        self.head =  nn.Sequential(
+                            nn.Linear(138,60), 
+                            nn.ReLU(inplace=True),
+                            nn.Dropout(0.2),
+                            nn.Linear(60, num_classes))
+        
+    
+    def forward(self, img, clinical):
+        img,_ = self.backbone(img)
+        img = self.fc1(img[:,0])
+        clinical =  self.fc2(clinical)
+        out = self.head(torch.cat([img, clinical], dim=1))
+        return out
+
+
 class VitMultiModalEnsenble(nn.Module):
     def __init__(self, clinical_feature_len,head='linear', num_classes=2,img_size=(64,256,256), patch_size=(8,32,32) ) -> None:
         super().__init__()
