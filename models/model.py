fecbdeb468d2b13e8fffe88e655db85c189f5200
mongshel <66589933+mongshel@users.noreply.github.com>
2023-03-19 17:33:26 +0900

    add gl

--- models/model.py
+++ models/model.py
@@ -54,7 +54,45 @@ class nnUnet(Generic_UNet):
         out = self.fc(img_em)
    
         return out
+    
+class nnUnetGL(Generic_UNet):
+    """Training with image only"""
 
+    def __init__(self, plans_path,num_classes=2, weight=None,fc_list = None):
+        
+        parameter = create_prameter(plans_path)
+        super().__init__(**parameter)
+
+        if weight :
+            self.load_state_dict(torch.load(weight)['state_dict'],strict=False)
+        
+        layers = []
+        in_features = 320 
+        
+        for i in range(len(fc_list)):
+            
+            out_features = fc_list[i]
+            
+            layers.append(nn.Linear(in_features, out_features))
+            layers.append(nn.LeakyReLU())
+
+            in_features = out_features
+            
+        layers.append(nn.Linear(in_features, num_classes))
+        
+        self.fc = nn.Sequential(*layers)
+    
+    def forward(self,x):
+
+        for d in range(len(self.conv_blocks_context) - 1):
+            x = self.conv_blocks_context[d](x)
+            if not self.convolutional_pooling:
+                x = self.td[d](x)
+        x = self.conv_blocks_context[-1](x)
+        img_em = F.adaptive_avg_pool3d(x, (1, 1,1)).view(x.shape[0],-1)
+        out = self.fc(img_em)
+   
+        return out
 class Vit(nn.Module):
     def __init__(self,num_classes=2,img_size=(64,256,256), patch_size=(8,32,32) ) -> None:
         super().__init__()
