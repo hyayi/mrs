5697ecb6c3c626a6139064ca60c0820c8634c1f1
mongshel <66589933+mongshel@users.noreply.github.com>
2023-04-03 18:50:17 +0900

    infer

--- datasets.py
+++ datasets.py
@@ -17,3 +17,22 @@ class MRSImaOlnyDataset(torch.utils.data.Dataset):
         image_data = self.transforms(image_dict)
     
         return image_data['img'], label
+
+
+class MRSImaOlnyDatasetInfer(torch.utils.data.Dataset):
+    def __init__(self, data_df,data_dir, transforms):
+        self.data_df = data_df
+        self.transforms = transforms
+        self.data_dir = data_dir
+
+    def __len__(self):
+        return len(self.data_df)
+
+    def __getitem__(self, index):
+        label = torch.as_tensor(self.data_df['label'][index])
+        
+        image_path = f"{self.data_dir}/{self.data_df['image'][index]}.nii.gz"
+        image_dict =  {'img' : image_path}
+        image_data = self.transforms(image_dict)
+    
+        return image_data['img'], label, self.data_df['image'][index]
