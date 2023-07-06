7aeab7de5e8de3bc9396d1fbcbabf1c199dea6fd
hyayi <98936899+hyayi@users.noreply.github.com>
2023-07-06 22:52:57 +0900

    Update model.py

--- models/model.py
+++ models/model.py
@@ -111,12 +111,12 @@ class EfficientNet(nn.Module):
         return x
         
 class Resnet50(nn.Module):
-    def __init__(self,input_chanel=1,pretrained=True,num_classes=2):
+    def __init__(self,input_chanel=1,pretrained=False,num_classes=2):
         super().__init__()
         self.model_ft = models.video.r3d_18(pretrained=pretrained)
-        prev_w = self.model_ft.stem[0].weight
+        #prev_w = self.model_ft.stem[0].weight
         self.model_ft.stem[0] = nn.Conv3d(input_chanel, 64, kernel_size=(3, 7, 7), stride=(1, 2, 2), padding=(1, 3, 3), bias=False)
-        self.model_ft.stem[0].weight.data = prev_w.data.sum(dim=1 ,keepdim=True)
+        #self.model_ft.stem[0].weight.data = prev_w.data.sum(dim=1 ,keepdim=True)
         self.model_ft.fc = nn.Linear(in_features=512, out_features=num_classes, bias=True)
     
     def forward(self,x):
